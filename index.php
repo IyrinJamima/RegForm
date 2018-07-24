d8d91c5c6da2e8959557e895a0ea842ef7e5e51c
IyrinJamima <iyrinjami810@gmail.com>
2018-07-24 22:58:17 +0530

    Insert

--- index.php
+++ index.php
@@ -6,7 +6,22 @@ if (mysqli_connect_errno()){
     if(isset($_REQUEST['submit'])!=''){
         if($_REQUEST['firstname']=='' || $_REQUEST['lastname']=='' || $_REQUEST['email']=='' || $_REQUEST['phonenumber']==''|| $_REQUEST['gender']=='' || $_FILES['image']==''){
             echo "please fill the empty field.";
-        }
+        }else{
+                $image=$_FILES['image']['name'];
+                $target="./images/".basename($image);
+    
+                $sql="insert into registration (firstname,lastname,email,phonenumber,gender,image) values('".$_REQUEST['firstname']."', '".$_REQUEST['lastname']."', '".$_REQUEST['email']."', '".$_REQUEST['phonenumber']."', '".$_REQUEST['gender']."', '".$image."')";
+                $res=$con->query($sql);
+                if(move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+                    $msg = "Image uploaded successfully";
+                }else{
+                    $msg = "Failed to upload image";
+                }
+                if($res){
+                }else{
+                    echo "There is some problem in inserting record";
+                }
+           }         
     }
 }
 ?>
@@ -14,7 +29,7 @@ if (mysqli_connect_errno()){
 <head>
 <title>REGISTRATION FORM</title>
 <body>
-<form name="registration" method="post" action="#">
+<form name="registration" method="post" action="#" enctype="multipart/form-data">
 FIRST-NAME:<input type="text" name="firstname" value=""></br>
 LAST-NAME:<input type="text" name="lastname" value=""></br>
 EMAIL-ID:<input type="text" name="email" value=""></br>
