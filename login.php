421d7a9fdb4b8427f1042b08f9c4dfea05233946
IyrinJamima <40312490+IyrinJamima@users.noreply.github.com>
2018-07-31 10:36:43 +0530

    Add files via upload
    Loginnew

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 /*ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);  */
+error_reporting(E_ALL);*/ 
 include('db.php');
 if(mysqli_connect_errno()){
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -11,11 +11,11 @@ if(mysqli_connect_errno()){
             echo "please fill the empty field.";
         }else{
             $sql="select * from registration where email_id = ".$_REQUEST['email_id'];
-            $result_set = mysqli_qurry($con, $sql);
+            $result_set = mysqli_query($con, $sql);
             $row = mysqli_fetch_assoc($result_set);
             if($row['password'] == $_REQUEST['password']){
-            $_SESSION['email'] = $_REQUEST['email_id'];
-            header('Location: home.php');
+            $_SESSION['email_id'] = $_REQUEST['email_id'];
+            header('Location: test.php');
              }
            }
          } 
@@ -30,13 +30,12 @@ if(mysqli_connect_errno()){
             <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Registration</b></div>
 				
             <div style = "margin:30px">
-               <form action = "#" method = "post" enctype="multipart/form-data" align="center">
+               <form action ="" method = "post" enctype="multipart/form-data" align="center">
                   <label>UserName  :</label><input type = "text" name = "email_id"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password"/><br/><br />
                   <input type="submit" name="submit" value="submit">
                   <input type ="button" name ="registration" onclick="javascript:registration_form()" value="registration">
                </form>
-               
                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
 					
             </div>
